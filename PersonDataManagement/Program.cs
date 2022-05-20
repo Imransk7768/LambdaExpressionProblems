7899d3cdd662cd9775a115f2b79d2f6644b2e92a
Imransk7768 <imransk7768@gmail.com>
2022-05-20 23:09:22 +0530

    [Imran] Added: Welcome to LambdaExpression Problems

---
+++ PersonDataManagement/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+    namespace PersonDataManagement
+{
+    public class Program
+    {
+        public static void Main(string[] arge)
+        {
+            Console.WriteLine("Welcome to LAMBDA EXPRESSION Problems");
+        }
+    }
+}
